2c6f7d741376d91f2619922b52f46cdc1c6d1130
ASHISH BISWAL <ashishbiswal1421@gmail.com>
2024-02-20 13:41:11 +0530

    sdfsfs

--- .cicd/gitversion.sh
+++ .cicd/gitversion.sh
@@ -138,7 +138,6 @@ update-pr)
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GITHUB_TOKEN}" \
         "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PR_NUMBER")
-    echo "pr_response='$pr_response'"
     current_pr_body=$(echo $pr_response | jq '.body')
     # echo "current_pr_body='$current_pr_body'"
     formatted_body=$(echo $current_pr_body | sed -e 'N;s/\n/\\n/g' -e 's/\\r\\n/\\n/g')
